2141cdba8a2cd521ff36927d5b622e79048eb491
Shubhrendu Tripathi <11039534+shubhtr@users.noreply.github.com>
2025-10-03 06:08:45 +0530

    tries added

--- set01/10-01-tries-01.js
+++ set01/10-01-tries-01.js
@@ -1,2 +1,15 @@
-console.log("hello world!");
 
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.isEndOfWord = false;
+    }
+}
+
+class Trie {
+    constructor() {
+        this.root = new TrieNode();
+    }
+
+    
+}
